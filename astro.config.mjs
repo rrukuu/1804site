04515c5fe5045bd6545d73dac19496ef34864013
rrukuu <t.ruku2005@gmail.com>
2026-02-14 21:34:59 +0900

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://http://localhost:4321',
+  site: 'https://1804site.pages.dev',
   output: 'static',
   build: {
     assets: '_assets',
